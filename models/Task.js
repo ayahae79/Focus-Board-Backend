aa402fd152e6eb3ffff21311a135f5585a71dd93
Ayah Ahmed <ayahahmed729@gmail.com>
2024-10-18 22:01:58 +0300

    status on the task

--- models/Task.js
+++ models/Task.js
@@ -4,6 +4,11 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   deadline: { type: Date, required: true },
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+  },
   course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" }, // Each task is linked to a course
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Assigned to a specific student
   event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" }, // Linked to an event for calendar tracking
