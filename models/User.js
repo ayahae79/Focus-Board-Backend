6fad05c0da325036ca0d6d0857a9fc5e5c0ae631
Ayah Ahmed <ayahahmed729@gmail.com>
2024-10-21 11:41:43 +0300

    profile done no bugs version

--- models/User.js
+++ models/User.js
@@ -14,6 +14,14 @@ const userSchema = new mongoose.Schema({
   roadmaps: [{ type: mongoose.Schema.Types.ObjectId, ref: "Roadmap" }],
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
   event: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
+  full_name: { type: String },
+  date_of_birth: { type: Date },
+  phone_number: { type: String },
+  student_id: { type: String },
+  major: { type: String },
+  year_of_study: { type: String },
+  gpa: { type: Number },
+  academic_advisor: { type: String },
 })
 
 module.exports = mongoose.model("User", userSchema)
