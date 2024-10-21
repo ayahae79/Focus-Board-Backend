d539b260aff19df9b356b18eb22411eaa51f4e81
aliHassanrabia <alihrabie@gmail.com>
2024-10-21 09:05:02 +0300

    new update

--- routes/Event.js
+++ routes/Event.js
@@ -1,10 +1,20 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const EventCtrl = require("../controllers/event")
+const EventCtrl = require('../controllers/event')
 
-router.post("/add", EventCtrl.createEvent)
-router.get("/event/:id", EventCtrl.getEvent)
-router.get("/allevents", EventCtrl.getAllEvents)
-router.delete("/event/:id", EventCtrl.deleteEvent)
+// Create a new event
+router.post('/add', EventCtrl.createEvent)
+
+// Get a specific event by ID
+router.get('/event/:id', EventCtrl.getEvent)
+
+// Get all events
+router.get('/allevents', EventCtrl.getAllEvents)
+
+// Update an existing event by ID
+router.put('/event/:id', EventCtrl.updateEvent)
+
+// Delete an event by ID
+router.delete('/event/:id', EventCtrl.deleteEvent)
 
 module.exports = router
