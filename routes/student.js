8daed29055cbcb82eddf5955917a85ebf0b64697
Dana <Danayousif867@gmail.com>
2024-10-21 16:01:00 +0300

    student remove

--- routes/student.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const StudentController = require('../controllers/StudentController');
-
-router.post('/', StudentController.createStudent);
-router.get('/:id', StudentController.getStudent);
-router.put('/:id', StudentController.updateStudent);
-
-module.exports = router;
